2be41e3409d38cc7c776fabeba7d5d2066eb5822
Eiren McLoughlin <eiren@gomobile.ie>
2016-02-23 12:32:29 +0000

    Small Error preventing switching page
    Silly mistake, forgot the ); in the index.js and that fixed the switching page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Secret Keeper' });
 });
 
+router.post('/login', function(req, res, next){
+    if(req.body.username == "Eiren" && req.body.password == "student"){
+        res.redirect("/secretsPage");
+    }
+    else{
+        res.redirect("/index");
+    }
+});
+
+
 module.exports = router;
+
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/*router.post('/login', function(req, res, next){
+   /* if(req.body.username == "Eiren" && req.body.password == "CreativeMutlimedia"){
+        req.redirect("/secretsPage", );
+    }*/
+   // res.send('Hello world');
+//}
+
+
 module.exports = router;
