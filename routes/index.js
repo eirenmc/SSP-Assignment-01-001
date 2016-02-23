65e612a6984af79ffbeb86f034a6b50d5c774b54
Eiren McLoughlin <eiren@gomobile.ie>
2016-02-23 12:46:40 +0000

    addition of comments
    Added comments to the current code but having trouble switching pages

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Secret Keeper' });
 });
 
-router.post('/login', function(req, res, next){
+//Rendering the index page
+router.get('index', function(req,res,next){
+   res.render('index'); 
+});
+
+/*
+  Deals with a post event from the index page and
+  if the username is Eiren and the password is student
+  then it s suppose to change to the secrets page, 
+  currently not doing this
+*/
+router.post('/index', function(req, res, next){
     if(req.body.username == "Eiren" && req.body.password == "student"){
         res.redirect("/secretsPage");
     }
+    //Otherwise render the index page
     else{
         res.redirect("/index");
     }
